a65400e7ce7577c5ff47793f0b1e18882be19cc2
OmarArias <omarxarias@gmail.com>
2023-03-28 20:22:55 -0400

    Some advance

--- app/src/main/java/com/example/proyectomobile/LoginActivity.kt
+++ app/src/main/java/com/example/proyectomobile/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.example.proyectomobile
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var btnLogin: Button
+    lateinit var btnFacebook: Button
+    lateinit var btnGoogle: Button
+    lateinit var welcomeText: TextView
+    lateinit var imageBackground: Image
+    lateinit var username: EditText
+    lateinit var password: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+             welcomeText = username + " " + password
+        }
     }
 }
